d9979504dcabbd9cdc093a5d23f159c1406ac776
Hager Neeb <BLAHBLAHneeb@users.noreply.github.com>
2020-09-14 00:02:38 -0600

    Added Player Capsule
    Player Capsule with wasd + arrow movement and mouse rotation. Change movement speed/sensitivity in movement script capsule < Mesh < Player_Character

---
+++ Bite at the Museum/Assets/Scripts/Character_Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Movement : MonoBehaviour
+{
+    public float Move_Speed; // Movement speed. Change this value in script component.
+    public float Rotate_Speed; // Rotation speed. Change this value in script component.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal and Vertical Movement
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * Move_Speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * Move_Speed);
+        // Vertical Rotation
+        transform.Rotate(0, Input.GetAxis("Mouse X") * Time.deltaTime * Rotate_Speed, 0);
+    }
+}
