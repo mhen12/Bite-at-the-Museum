ecc9a8279968880a3d0fba9d452d8bdc4dd0cfe4
mhen12 <43431701+mhen12@users.noreply.github.com>
2020-09-16 02:54:02 -0600

    Added RexRig with a test walk animation

---
+++ Bite at the Museum/Assets/Scripts/Animation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("w"))
+        {
+            anim.SetBool("Walk", true);
+            anim.SetBool("Idle", false);
+        }
+
+        if (Input.GetKeyUp("w"))
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Walk", false);
+        }
+    }
+}
