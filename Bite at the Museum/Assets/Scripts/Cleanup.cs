82b511b6d1a9a39e7aa4928735438463c5060f2d
Melissa Kirkpatrick <melissa@waterglen.net>
2020-09-17 10:23:22 -0600

    Made boxes explode when they collide with the character.

---
+++ Bite at the Museum/Assets/Scripts/Cleanup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cleanup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(destructTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator destructTimer()
+    {
+        yield return new WaitForSeconds(8);
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
+}
