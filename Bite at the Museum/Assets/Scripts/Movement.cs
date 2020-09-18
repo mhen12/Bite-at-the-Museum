f8bcba856a9cf7ecde66f9ef3ea166bb7d8cffe8
bennv <iambatman251@gmail.com>
2020-09-18 14:33:13 -0600

    Movement
    Rex now turns to face the direction he's walking in. Cinemachine is installed and a camera is there but needs updating. Animation now plays whenever Rex is moving. Removed animation and character movement components as they're both now in the movement component.

---
+++ Bite at the Museum/Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+    public float turnSmooth = 0.1f;
+    float turnVelocity;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            anim.SetBool("Walk", true);
+            anim.SetBool("Idle", false);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnSmooth);
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            Vector3 movDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(movDir.normalized * speed * Time.deltaTime);
+        }
+        else
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Walk", false);
+        }
+    }
+}
