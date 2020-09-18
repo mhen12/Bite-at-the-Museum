f6f479544ed85fb96f2dca4729db22f6db4c1a43
bennv <iambatman251@gmail.com>
2020-09-18 11:53:50 -0600

    set up .sln

---
+++ Bite at the Museum/Assets/Scripts/Dialogue_Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue_Manager : MonoBehaviour
+{
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogue_Active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogue_Active && Input.GetKeyDown(KeyCode.Space))
+        {
+            dBox.SetActive(false);
+            dialogue_Active = false;
+        }
+    }
+
+    public void ShowBox(string dialogue)
+    {
+        dialogue_Active = true;
+        dBox.SetActive(true);
+        dText.text = dialogue;
+    }
+
+    public void HideBox()
+    {
+        dialogue_Active = false;
+        dBox.SetActive(false);
+    }
+}
