4485f3a47683de2e77128d0c6eaf4f3b1ed8331d
Hager Neeb <BLAHBLAHneeb@users.noreply.github.com>
2020-09-18 01:03:17 -0600

    Added Simple Security Guard NPC with a Dialogue System.
    Added Prefabs: Guard, Dialogue Manager
    
    Added Scripts: Dialogue_Manager, Dialogue_Holder
    
    Every scene with dialogue needs the Dialogue Manager prefab added. Any character that needs dialogue needs the Dialogue_Holder script component added (this is also where you set the displayed text).

---
+++ Bite at the Museum/Assets/Scripts/Dialogue_Holder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue_Holder : MonoBehaviour
+{
+
+    public string dialogue;
+    private Dialogue_Manager dMan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dMan = FindObjectOfType<Dialogue_Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //if (Input.GetKeyDown(KeyCode.Space)) // should make you press space first, but not working.
+            //{
+                dMan.ShowBox(dialogue);
+            //}
+        }
+    }
+}
