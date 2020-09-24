60c65bc2f5310975d856908271d6860a6da82817
mhen12 <43431701+mhen12@users.noreply.github.com>
2020-09-23 23:31:56 -0600

    Added head movement controller

---
+++ Bite at the Museum/Assets/Scripts/HeadController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class HeadController : MonoBehaviour
+{
+    public Transform offset;
+
+    void Update()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x - Screen.width/2, Input.mousePosition.y - Screen.height/2, offset.transform.position.z);
+        this.transform.position = mousePosition;
+    }
+
+}
---
+++ Bite at the Museum/Assets/Scripts/HeadController2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController2 : MonoBehaviour
+{
+    private float speed = 5;
+    private float targetRadius = 1;
+    private Ray ray;
+    private Vector3 target;
+      
+     void Start()
+    {
+        //    rigidbody.freezeRotation = true;
+        target = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        target.y = transform.position.y;
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            target = hit.point;
+        }
+
+        if (Vector3.Distance(transform.position, target) > targetRadius)
+        {
+            transform.LookAt(target);
+            transform.Translate(0, 0, speed);
+        }
+    }
+}
