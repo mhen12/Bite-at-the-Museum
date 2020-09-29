d58feaec4ce51a9328d5037c9737e907867fb675
mhen12 <43431701+mhen12@users.noreply.github.com>
2020-09-28 20:32:34 -0600

    Added tophat and changed jaw collider

---
+++ Bite at the Museum/Assets/Scripts/Accessories.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accessories : MonoBehaviour
+{
+    private GameObject foundItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Tophat"))
+        {
+            foundItem = col.gameObject;
+            Pickup(foundItem);
+        }
+    }
+
+    public void Pickup(GameObject Obj)
+    {
+        Obj.transform.position = this.transform.position;
+        Obj.transform.position = Obj.transform.position + new Vector3(0, 3, 0);
+        Obj.transform.parent = this.transform;
+    }
+}
